ccb82787fb5057702c28a0aea6906ced42b00e88
Giridhar Shanbhag <geeks2022@gmail.com>
2020-12-25 14:44:41 +0530

    Converted to stateful components

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { Component } from "react"
 import {
   Grid,
   Header,
@@ -11,45 +11,74 @@ import {
 import { Link } from "react-router-dom"
 import "../../styles/App.css"
 
-function Login() {
-  return (
-    <Grid textAlign="center" verticalAlign="middle" className="app">
-      <Grid.Column style={{ maxWidth: 450 }} textAlign="left">
-        <Header as="h2" icon color="blue" textAlign="center">
-          <Icon name="ticket" color="blue" />
-          Login to ShowTym
-        </Header>
-        <Form size="large">
-          <Segment stacked>
-            <Form.Input
-              fluid
-              name="email"
-              placeholder="Enter your registered email"
-              label="Enter your E-mail"
-              icon="mail"
-              iconPosition="left"
-              type="email"
-            />
-            <Form.Input
-              fluid
-              name="password"
-              placeholder="Enter your password"
-              label="Enter your password"
-              icon="lock"
-              iconPosition="left"
-              type="password"
-            />
-            <Button fluid color="blue" size="large">
-              Sign In
-            </Button>
-          </Segment>
-        </Form>
-        <Message style={{ textAlign: "center" }}>
-          New user? Create an account for free <Link to="/signup">here</Link>
-        </Message>
-      </Grid.Column>
-    </Grid>
-  )
+class Login extends Component {
+  state = { email: "", password: "", loading: false, errors: [] }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+  }
+
+  displayErrors = errors =>
+    errors.map((error, i) => <p key={i}>{error.message}</p>)
+
+  render() {
+    const { email, password, loading, errors } = this.state
+
+    return (
+      <Grid textAlign="center" verticalAlign="middle" className="app">
+        <Grid.Column style={{ maxWidth: 450 }} textAlign="left">
+          <Header as="h2" icon color="blue" textAlign="center">
+            <Icon name="ticket" color="blue" />
+            Login to ShowTym
+          </Header>
+          <Form size="large" onSubmit={this.handleSubmit}>
+            <Segment stacked>
+              <Form.Input
+                fluid
+                name="email"
+                value={email}
+                onChange={this.handleChange}
+                placeholder="Enter your registered email"
+                label="Enter your E-mail"
+                icon="mail"
+                iconPosition="left"
+                type="email"
+              />
+              <Form.Input
+                fluid
+                name="password"
+                value={password}
+                onChange={this.handleChange}
+                placeholder="Enter your password"
+                label="Enter your password"
+                icon="lock"
+                iconPosition="left"
+                type="password"
+              />
+              <Button
+                fluid
+                color="blue"
+                size="large"
+                className={loading ? "loading" : ""}
+              >
+                Sign In
+              </Button>
+            </Segment>
+          </Form>
+          {errors.length > 0 && (
+            <Message error>Error: {this.displayErrors(errors)}</Message>
+          )}
+          <Message style={{ textAlign: "center" }}>
+            New user? Create an account for free <Link to="/signup">here</Link>
+          </Message>
+        </Grid.Column>
+      </Grid>
+    )
+  }
 }
 
 export default Login
--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { Component } from "react"
 import {
   Grid,
   Header,
@@ -7,96 +7,173 @@ import {
   Segment,
   Button,
   Message,
-  Dropdown,
 } from "semantic-ui-react"
 import { Link } from "react-router-dom"
 import "../../styles/App.css"
 
-function Signup() {
-  return (
-    <Grid textAlign="center" verticalAlign="middle" className="app">
-      <Grid.Column style={{ maxWidth: 600 }} textAlign="left">
-        <Header as="h2" icon color="blue" textAlign="center">
-          <Icon name="ticket" color="blue" />
-          Register for ShowTym
-        </Header>
-        <Form size="large">
-          <Segment stacked>
-            <Form.Group widths="equal">
+class Signup extends Component {
+  state = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    contact: "",
+    day: "",
+    month: "",
+    year: "",
+    loading: false,
+    errors: [],
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+  }
+
+  displayErrors = errors =>
+    errors.map((error, i) => <p key={i}>{error.message}</p>)
+
+  render() {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      confirmPassword,
+      contact,
+      day,
+      month,
+      year,
+      loading,
+      errors,
+    } = this.state
+
+    return (
+      <Grid textAlign="center" verticalAlign="middle" className="app">
+        <Grid.Column style={{ maxWidth: 600 }} textAlign="left">
+          <Header as="h2" icon color="blue" textAlign="center">
+            <Icon name="ticket" color="blue" />
+            Register for ShowTym
+          </Header>
+          <Form size="large" onSubmit={this.handleSubmit}>
+            <Segment stacked>
+              <Form.Group widths="equal">
+                <Form.Input
+                  fluid
+                  name="firstName"
+                  placeholder="Enter your First name"
+                  label="Enter your first name"
+                  value={firstName}
+                  onChange={this.handleChange}
+                  icon="user"
+                  type="text"
+                  iconPosition="left"
+                />
+                <Form.Input
+                  fluid
+                  name="lastName"
+                  value={lastName}
+                  onChange={this.handleChange}
+                  placeholder="Enter your Last name"
+                  label="Enter your last name"
+                  type="text"
+                />
+              </Form.Group>
               <Form.Input
                 fluid
-                name="firstName"
-                placeholder="Enter your First name"
-                label="Enter your first name"
-                icon="user"
-                type="text"
+                name="email"
+                value={email}
+                onChange={this.handleChange}
+                placeholder="Enter your email"
+                label="Enter your E-mail"
+                icon="mail"
                 iconPosition="left"
+                type="email"
               />
               <Form.Input
                 fluid
-                name="lastName"
-                placeholder="Enter your Last name"
-                label="Enter your last name"
+                name="contact"
+                placeholder="Enter your mobile number"
+                value={contact}
+                onChange={this.handleChange}
+                label="Enter your contact number"
+                icon="mobile"
+                iconPosition="left"
                 type="text"
               />
-            </Form.Group>
-            <Form.Input
-              fluid
-              name="email"
-              placeholder="Enter your email"
-              label="Enter your E-mail"
-              icon="mail"
-              iconPosition="left"
-              type="email"
-            />
-            <Form.Input
-              fluid
-              name="contact"
-              placeholder="Enter your mobile number"
-              label="Enter your contact number"
-              icon="mobile"
-              iconPosition="left"
-            />
-            <Form.Field label="Date of Birth" />
-            <Form.Group widths="equal">
-              <Dropdown placeholder="Select Date" name="day" fluid selection />
-              <Dropdown
-                placeholder="Select Month"
-                name="month"
+              <Form.Field label="Date of Birth" />
+              <Form.Group widths="equal">
+                <Form.Input
+                  fluid
+                  name="day"
+                  value={day}
+                  onChange={this.handleChange}
+                  placeholder="Enter date"
+                  type="text"
+                />
+                <Form.Input
+                  fluid
+                  name="month"
+                  value={month}
+                  onChange={this.handleChange}
+                  placeholder="Enter month"
+                  type="text"
+                />
+                <Form.Input
+                  fluid
+                  name="year"
+                  value={year}
+                  onChange={this.handleChange}
+                  placeholder="Enter year"
+                  type="text"
+                />
+              </Form.Group>
+              <Form.Input
                 fluid
-                selection
+                name="password"
+                value={password}
+                onChange={this.handleChange}
+                placeholder="Enter your password"
+                label="Enter your password"
+                icon="lock"
+                iconPosition="left"
+                type="password"
               />
-              <Dropdown placeholder="Select Year" name="year" fluid selection />
-            </Form.Group>
-            <Form.Input
-              fluid
-              name="password"
-              placeholder="Enter your password"
-              label="Enter your password"
-              icon="lock"
-              iconPosition="left"
-              type="password"
-            />
-            <Form.Input
-              fluid
-              name="password"
-              placeholder="Re-enter your password"
-              label="Confirm your password"
-              icon="repeat"
-              iconPosition="left"
-              type="password"
-            />
-            <Button fluid color="blue" size="large">
-              Register
-            </Button>
-          </Segment>
-        </Form>
-        <Message style={{ textAlign: "center" }}>
-          Existing user? Login <Link to="/login">here</Link>
-        </Message>
-      </Grid.Column>
-    </Grid>
-  )
+              <Form.Input
+                fluid
+                name="password"
+                value={confirmPassword}
+                onChange={this.handleChange}
+                placeholder="Re-enter your password"
+                label="Confirm your password"
+                icon="repeat"
+                iconPosition="left"
+                type="password"
+              />
+              <Button
+                fluid
+                color="blue"
+                size="large"
+                className={loading ? "loading" : ""}
+              >
+                Register
+              </Button>
+            </Segment>
+          </Form>
+          {errors.length > 0 && (
+            <Message error>Error: {this.displayErrors(errors)}</Message>
+          )}
+          <Message style={{ textAlign: "center" }}>
+            Existing user? Login <Link to="/login">here</Link>
+          </Message>
+        </Grid.Column>
+      </Grid>
+    )
+  }
 }
 
 export default Signup
